eb25dc389633214c81147476a305050dfed0d7d0
Dmytro Butko <dmitri.butko@ukr.net>
2021-09-02 12:36:21 +0300

    Fisrt commit of project

---
+++ src/main/java/com/seabattlespring/springseabattle/SeaBattleSpringApplication.java
@@ -0,0 +1,13 @@
+package com.seabattlespring.springseabattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaBattleSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeaBattleSpringApplication.class, args);
+    }
+
+}
